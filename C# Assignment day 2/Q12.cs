2a42e4b931532b6c125000b584d8e1f36b4eacc2
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 15:38:43 +0530

    Charecter to sports marking

---
+++ C# Assignment day 2/Q12.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q12
+{
+    internal class Program
+    {
+        /*
+character   Sports
+c           Cricket
+f           Football
+h           Hockey
+t           Tennis
+b           Badminton    */
+        static void Main(string[] args)
+        {
+        switchLabel:
+            Console.WriteLine("Enter the charecter to match the sports");
+            char c=char.Parse(Console.ReadLine());
+            string sports=null;
+            
+            switch (c)
+            {
+                case 'c':
+                    sports = "Cricket";
+                    break;
+                case 'f':
+                    sports = "Football";
+                    break;
+                case 'h':
+                    sports = "hockey";
+                    break;
+                case 't':
+                    sports = "tennis";
+                    break;
+                case 'b':
+                    sports = "badminton";
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, please try again");
+                    goto switchLabel;
+                    
+
+            }
+            Console.WriteLine("The corrsponding sports is "+sports);
+            Console.ReadLine();
+        }
+    }
+}
