d5fb05f0a09bd1e7f77f0efc7acbfee7b43238c3
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 12:15:24 +0530

    Print prime numbers below 100

---
+++ C# Assignment day 2/Q8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q8
+{
+    internal class Program
+    {
+        static bool checkPrime(int n)
+        {
+            if(n == 0) return false;
+            if(n == 1) return false;
+            if(n == 2) return true;
+            for(int i = 2; i < n; i++)
+            {
+                if (n%i==0)
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+       
+
+        static void Main(string[] args)
+        {
+            //Print all prime numbers below 100
+            for(int i = 0;i<100;i++)
+            {
+                bool isprime = checkPrime(i);
+                if(isprime) { Console.WriteLine(i); }
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
