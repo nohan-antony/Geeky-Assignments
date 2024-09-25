6384f6369b8fdd30df15614df07d50c7e8bf7356
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 10:58:04 +0530

    Print odd numbers less than 50 using while loop

---
+++ C# Assignment day 2/Q2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            while (number < 50) { 
+            if (number%2 !=0) {
+                    Console.WriteLine(number);
+                }
+            number++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
