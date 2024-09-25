6e02ae47f1c0da191c411b0c5ecf97779ec36ce8
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 15:45:46 +0530

    Pattern Printing

---
+++ C# Assignment day 2/Q13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q13
+{
+    internal class Program
+    {
+        /* print pattern
+         
+*
+**
+***
+****
+*****
+
+         */
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
