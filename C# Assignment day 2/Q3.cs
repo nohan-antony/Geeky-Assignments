9d9eab59c163b4915f74f89db9ed1c74ae4464e2
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 11:15:49 +0530

    Large amount 3 numbers

---
+++ C# Assignment day 2/Q3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Large amount 3 numbers
+            Console.WriteLine("Input First number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the secound number");
+            int num2= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number");
+            int num3= int.Parse(Console.ReadLine());
+
+            int largest = 0;
+            if (num1 > num2) {
+                largest = num1;
+            }
+            else
+            {
+                largest = num2;
+            }
+            if (num3 > largest)
+            {
+                largest = num3;
+            }
+
+            Console.WriteLine(largest + " is the largest amoung the three");
+            Console.ReadLine();
+        }
+    }
+}
