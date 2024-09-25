18fb9380efee7498160deafe2e6fdbb8b4e8e518
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 13:39:07 +0530

    Palindrome

---
+++ C# Assignment day 2/Q10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q10
+{
+    internal class Program
+    {
+        //check palindrome
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Word to check palindrome");
+            string str=Console.ReadLine();
+            int n=str.Length;
+            bool b=false;
+            for (int i=0; i<n; i++)
+            {
+                if (char.ToLower(str[i]) != char.ToLower(str[n - 1 - i])){
+                    b= true;
+                    break;
+
+                }
+            }
+
+            if (!b)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
