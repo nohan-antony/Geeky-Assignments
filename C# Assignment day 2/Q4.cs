b69017a27cb5ac999471868138d30d6d06d3b9a1
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 11:26:56 +0530

    Reverse a number

---
+++ C# Assignment day 2/Q4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class Program
+    {
+        //Reverse of a number
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number to Reverse");
+            int n=int.Parse(Console.ReadLine());
+            int reverseNumber = 0;
+            while(n>0)
+            {
+                reverseNumber=reverseNumber*10+n%10;
+                n = n / 10;
+            }
+            Console.WriteLine(reverseNumber);
+            Console.ReadLine();
+        }
+    }
+}
