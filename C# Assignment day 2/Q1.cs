2debd1d9b8cd26803310bdc57774404d28312ec4
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 10:42:53 +0530

    Print First 20 numbers

---
+++ C# Assignment day 2/Q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                for (int i = 1; i <= 20; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            Console.ReadLine();
+            }
+        }
+
+    }
+
+
