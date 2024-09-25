cbcf58bf850db42afdd41e5dd9c6755b9c17c785
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 11:31:17 +0530

    Sum of digits of a number

---
+++ C# Assignment day 2/Q5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class Program
+    {
+        //sum of digits of number
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number to find the sum of digits");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (n > 0)
+            {
+                sum = sum  + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
