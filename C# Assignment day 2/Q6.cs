02595b0a51bc2c84a5e8a9584bd7c1e87e644184
Nohan Antony <nohan.antony@geekywolf.com>
2024-09-25 11:51:50 +0530

    Sum of the squares of the digits of a number

---
+++ C# Assignment day 2/Q6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number to find sum of sqaures of digits");
+            int n = int.Parse(Console.ReadLine());
+            int sumSq = 0;
+            while (n > 0)
+            {
+                sumSq = sumSq  + (n % 10)* (n % 10);
+                n = n / 10;
+            }
+            Console.WriteLine(sumSq);
+            Console.ReadLine();
+
+        }
+    }
+}
